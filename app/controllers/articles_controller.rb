207a1fc581158d50bccdb5657298cfd6adfcf7b3
Dan Berger <dsberger@gmail.com>
2015-02-18 15:12:56 -0800

    working #show route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # def show
-    # @article = Article
-  # end
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(whitelisted_article_params)
