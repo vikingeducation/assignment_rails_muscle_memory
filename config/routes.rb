cec0090456af192e4114011f6904dfdaeb73be89
Dan Berger <dsberger@gmail.com>
2015-02-18 14:43:51 -0800

    new article form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :posts, only: [:new, :create, :show, :index]
+  resources :articles, only: [:new, :create, :show, :index]
 end
