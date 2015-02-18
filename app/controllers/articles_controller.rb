5f605c5bbb62f30fbd4e94ed1c2f36693ca77125
Dan Berger <dsberger@gmail.com>
2015-02-18 15:08:29 -0800

    working (I think) create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,24 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  # def show
+    # @article = Article
+  # end
+
+  def create
+    @article = Article.new(whitelisted_article_params)
+    if @article.save
+      redirect_to article_path(@article[:id])
+      flash[:success] = "Article saved."
+    else
+      flash[:error] = "Article was not saved."
+      render :new
+    end
+  end
+
+  def whitelisted_article_params
+    params.require(:article).permit(:title,:body)
   end
 end
