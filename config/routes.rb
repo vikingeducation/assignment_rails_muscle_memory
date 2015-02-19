43dad047d0f59e9fc9ae4c38a1dd3574067e6553
Dan Berger <dsberger@gmail.com>
2015-02-18 19:42:56 -0800

    edits during Thursday scrum

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:new, :create, :show, :index]
+  resources :articles, only: [:new, :create, :show, :index, :destroy]
 end
