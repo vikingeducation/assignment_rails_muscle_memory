c69f80430e454f2afbfbc3d4da8f63a465a894b2
Daniel Au <dakmau@gmail.com>
2015-02-20 07:34:59 -0500

    Formfix 3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,21 @@
 class ArticlesController < ApplicationController
+
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(whitelisted_post_params)
+    if @article.save
+      redirect_to article_path(@post.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def whitelisted_post_params
+    params.require(:article).permit(:title,:body)
   end
 end
