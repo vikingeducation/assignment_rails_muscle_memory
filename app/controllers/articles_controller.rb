dcc48f54f1b2c24327f1dd9edee5e2f16d5434c1
niconan <nick.hax@gmail.com>
2015-04-30 23:05:32 -0700

    making articles works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,21 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article '#{@article.title}' created!"
+      redirect_to @article
+    else
+      flash.now[:error] = "Problem creating the article!"
+      render :index
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
