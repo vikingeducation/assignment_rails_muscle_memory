336512d00282a3e70d6b19696b3cf69b7076a88f
Adam <adam.kinson@gmail.com>
2015-07-01 23:33:46 -0400

    finish create and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,31 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
+  end
 
+
+  def create
+    @article = Article.new(whitelisted_post_params)
+
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+
+  end
+
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+
+  def whitelisted_post_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
