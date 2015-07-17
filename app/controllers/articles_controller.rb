912fff4359af747e4437e439d5db40a5daf7a233
David <davidmeza@outlook.com>
2015-07-17 13:53:44 -0400

    finish creating new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,23 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def new
 
   end
 
-  # def index
+  def index
+    @articles = Article.all
+  end
 
-  # end
+  def create
+    @article = Article.new(allowed_params)
+    @article.save
+    redirect_to article_path(@article.id)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root :to => "articles#index"
-  resources :articles, :only => [:index, :create, :new]
+  resources :articles, :only => [:index, :create, :new, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
