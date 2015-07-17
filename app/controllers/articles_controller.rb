918292e10881131e0ae97fcbe9338cd4c3f3ee94
facingsouth <xin.d.liu@gmail.com>
2015-07-17 10:13:13 -0700

    fix article create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,16 @@ class ArticlesController < ApplicationController
 
   def new
   end
+
+  def create
+    @article = Article.new(post_params)
+    @article.save
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:articles).permit(:title, :body)
+  end
 end
