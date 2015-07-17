c9afe1e090b36096f4ee049ec18d36b040c73eb3
shadefinale <shadefinale@gmail.com>
2015-07-17 11:58:51 -0500

    Begin on edit functionality.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @new_article = Article.new
+    @new_article.title = params[:title]
+    @new_article.body = params[:body]
+    if @new_article.save
+      redirect_to(article_path(@new_article))
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+end
