8c1d1ac646e3465a853a85ef33e2d1a7a51b966a
Garrett <garrettweinert@gmail.com>
2015-07-17 10:08:50 -0700

    Working on create method

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+
+  def new
+
+  end
+
+  # def index
+
+  # end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => "articles#index"
+  resources :articles, :only => [:index, :create, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
