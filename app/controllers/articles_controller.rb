feee02aab7ce0fbfd46a2ace8a775e3dca8fdff0
Chris Scavello <bideowego@gmail.com>
2015-09-17 02:10:46 -0400

    Add destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,15 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 		@article = Article.new
 	end
 
-	def show
+	def edit
 		@article = Article.find(params[:id])
 	end
 
@@ -20,6 +24,24 @@ class ArticlesController < ApplicationController
 			flash.now[:error] = 'Error: article not created'
 			render :new
 		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update_attributes(article_params)
+			flash[:success] = 'Article updated!'
+			redirect_to article_path(@article.id)
+		else
+			flash.now[:error] = 'Error: article not updated'
+			render :edit
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:success] = 'Article destroyed!'
+		redirect_to articles_path
 	end
 
 	def article_params
