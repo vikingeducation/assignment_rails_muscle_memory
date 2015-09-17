b82447615c6ab234649ab21890a5211d6ade798c
Chris Scavello <bideowego@gmail.com>
2015-09-17 01:23:44 -0400

    Add create, new, show Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,23 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = 'Article created!'
+			redirect_to article_path(@article.id)
+		else
+			flash.now[:error] = 'Error: article not created'
+			render :new
+		end
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
 end
