14df4a0ef78a5e908e959767f43875303c76d4fc
Sia <siakaramalegos@gmail.com>
2015-12-17 08:00:16 -0600

    Adds new method and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'articles/new'
+
+  root 'articles#new'
+
+  resources :articles, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
