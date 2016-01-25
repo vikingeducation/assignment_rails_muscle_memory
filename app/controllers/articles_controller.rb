3bee834018adb0c029a14676813c7e6a24036b79
Kelsey James <kelseybjames@gmail.com>
2016-01-25 11:03:26 -0800

    Added form submission

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,21 @@
 class ArticlesController < ApplicationController
 
-def new
+  def new
 
-end
+  end
 
+  def create
+    @article = Article.new(whitelisted_post_params)
+    if @article.save
+      redirect_to :show
+    end
+  end
+
+
+  private
+
+  def whitelisted_post_params
+    params.require(:article).permit(:title, :body)
+  end
 
 end
