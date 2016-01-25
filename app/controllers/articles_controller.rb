8f69c4b6aa0c28a86c0dac0202f1ba029188b9cb
Andrew Baik <and.baik@gmail.com>
2016-01-25 14:03:44 -0500

    completed create controller method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    @article = Article.new(title: params[:title], body: params[:body])
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+
+    end
+  end
 end
