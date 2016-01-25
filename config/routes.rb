0c560ac591caaaefefdde2a92c1f2d87f0b5a0b9
Sam O'Keefe <samok13@gmail.com>
2016-01-25 12:01:42 -0800

    through index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'articles/new'
 
-  resources :articles, only: [:new, :create, :show]
+  resources :articles, only: [:new, :create, :show, :index, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
