b48858a910fd7df9cb21b824e4785775d170f268
Steven Chang <prime_pork@hotmail.com>
2016-02-23 16:21:43 +1000

    #update is functional

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,4 +25,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update({:title => params[:title], :body => params[:body]})
+
+    redirect_to @article
+  end
+
 end
