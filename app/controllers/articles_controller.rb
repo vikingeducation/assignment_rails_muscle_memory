292232db6d28767554c9f40a2b145d2bdbedb42d
JeremyVe <jepschx@gmail.com>
2016-04-22 16:02:59 +0800

    index done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def article_params
     params.require(:article).permit( :title, :body )
   end
