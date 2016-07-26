f6b99ac2747dc62e7878f2356f82cf88950d4f33
David Jiang <davidjgrey@gmail>
2016-07-26 10:41:26 -0700

    working on update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,4 +18,15 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params)
+    redirect_to article_path(@article)
+  end
+
 end
