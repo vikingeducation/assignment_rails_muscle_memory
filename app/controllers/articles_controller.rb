51e7f260cca4ef555d470e5209b39fd722ab3d93
Adrian Mui <adrian.mui@gmail.com>
2016-07-26 14:02:42 -0400

    navbar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
-
-  def new
-    
+  def new  
   end
 
   def create
@@ -14,7 +12,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article =Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def index 
+    @article = Article.all
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # resources :articles
   root to: 'articles#index'
-  get 'new' => 'articles#new'
+  resources :articles, :except => [:update, :destroy, :create]
 
-  post 'articles/new' =>  'articles#create'
-  get 'articles/:id' => 'articles#show', as: 'article'
+
+  
+  # get 'new' => 'articles#new'
+  # post 'articles/new' =>  'articles#create'
+  # get 'articles/:id' => 'articles#show', as: 'article'
 
 end
