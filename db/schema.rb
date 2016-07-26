2d564e2f1e1c36c50fd062c8e7d235b20f411792
Hannah Squier <hannahsquier@gmail.com>
2016-07-26 11:21:47 -0700

    add schmarticles scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726162908) do
+ActiveRecord::Schema.define(version: 20160726181754) do
 
   create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schmarticles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
