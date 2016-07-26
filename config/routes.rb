1c99a46b839f9195da087b0c2fb78ec75ff11546
Morgan Martin <morganm5201@gmail.com>
2016-07-26 14:57:57 -0400

    make editing work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 AssignmentRailsMuscleMemory::Application.routes.draw do
   root to: 'articles#new'
-  resources :articles, :only => [:new, :create, :show, :index]
+  resources :articles, :only => [:new, :create, :show, :index, :edit, :update]
 end
