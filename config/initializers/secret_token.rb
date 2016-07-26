29915dc3d1fa5fd58010b06d107a488ffef21367
Morgan Martin <morganm5201@gmail.com>
2016-07-26 12:18:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssignmentRailsMuscleMemory::Application.config.secret_key_base = '51fdecac301f35be628404ab464b879179f5544302fcba5fe4dd4ae24556f11a5a0b365a39dd9d951068da17b934779c207caa4d8ee6641255285d9fc4b9a9ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssignmentRailsMuscleMemory::Application.config.session_store :cookie_store, key: '_assignment_rails_muscle_memory_session'
