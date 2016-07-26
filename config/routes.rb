5fc6975abae91e7ef2fafab26bff96c690f13561
cjvirtucio87 <cjv287@gmail.com>
2016-07-26 10:03:13 -0700

    Added index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root :to => "articles#new"
-  resources :articles, :only => [:new, :create, :show]
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
