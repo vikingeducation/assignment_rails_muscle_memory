879fcf3c62f51cb52b7e5835ac31ded9e3d25e9d
Alex Lach <alexglach@gmail.com>
2016-07-26 13:18:34 -0400

    done with create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,22 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(whitelisted_article_params)
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+
+  end
+
+
+  private
+
+  def whitelisted_article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
