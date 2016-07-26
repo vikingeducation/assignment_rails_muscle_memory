32e12f548d9b3b7f4006c6d06c46cae614ed00a1
Alex Lach <alexglach@gmail.com>
2016-07-26 14:03:13 -0400

    done with edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.title = whitelisted_article_params[:title]
+    @article.body = whitelisted_article_params[:body]
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :edit
+    end
+
+  end
+
 
   private
 
