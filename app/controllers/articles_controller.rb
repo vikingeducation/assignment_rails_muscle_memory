2e3051e5b7ab032554f27ead5e13f680d0d9599a
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-26 09:47:44 -0700

    Working on create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
