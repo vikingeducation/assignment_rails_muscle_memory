9aa13fc46853db776267feb6ed68baaf0b66c08e
Vishal Lama <vishal.lama@gmail.com>
2016-07-26 11:12:50 -0600

    Add #create for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,22 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = "Article created!"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Could not create article"
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
