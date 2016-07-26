b3ff9aa276483bf9d24fb41b14e653864ac6a42a
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-26 12:53:22 -0400

    Made model, views and controller for article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
