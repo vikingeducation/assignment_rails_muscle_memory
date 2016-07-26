930927fb97c46d9136800d6a05caf2d77afc770c
Adrian Mui <adrian.mui@gmail.com>
2016-07-26 16:03:13 -0400

    fin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id])
-     @article.destroy
+    @article.destroy
      redirect_to root_path
   end
 
