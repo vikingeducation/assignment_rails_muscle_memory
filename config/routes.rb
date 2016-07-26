92298c917c1878ce7a547a076859eb97f4e92733
Graham Turner <turnertgraham@gmail.com>
2016-07-26 12:47:56 -0400

    wokrking on show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root :to => "articles#new"
-  resources :articles, :only => [:new, :create]
+  resources :articles, :only => [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
