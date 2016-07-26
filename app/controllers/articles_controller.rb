59825d8f3232002cb92678fe4e3281c21f06e217
Morgan Martin <morganm5201@gmail.com>
2016-07-26 14:04:46 -0400

    fix showing, input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,25 @@
 class ArticlesController < ApplicationController
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(whitelisted_article_params)
-    if @article.save 
-      redirect_to articles_path(@article)
+    if @article.save
+      redirect_to article_path(@article)
     else
       render :new
     end
-  end
-
-  private 
-
-  def whitelisted_article_params
-    params.require(:article).permit(:title, :body, :id)
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
+  private
+
+    def whitelisted_article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 AssignmentRailsMuscleMemory::Application.routes.draw do
   root to: 'articles#new'
-  get  '/articles/new' => 'articles#new'
-  post '/articles/new' => 'articles#create'
-  get '/articles/:id' => 'articles#show'
+  resources :articles
 end
