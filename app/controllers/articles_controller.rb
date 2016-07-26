08a2fa7390a501fd8bc1dd9fb0a34e3abb841092
Adrian Mui <adrian.mui@gmail.com>
2016-07-26 13:12:59 -0400

    create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,17 @@ class ArticlesController < ApplicationController
   def new
     
   end
+
+  def create
+    @article = Article.new({:body => params[:body], 
+                                         :title   => params[:title] })
+    @article.save
+    flash[:success]
+
+    redirect_to '/new'
+  end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  #root to: 'articles#index'
   get 'new' => 'articles#new'
+
+  post 'create' =>  'articles#create'
+
 end
