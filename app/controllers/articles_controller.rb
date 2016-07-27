d4d1ba47936ac14c162eac6c68a3b42976db6a08
schoetlr <srlrs20020@gmail.com>
2016-07-27 17:26:24 -0400

    add form

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,8 @@
+class ArticlesController < ApplicationController
+
+  def new
+
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'articles#new'
+  resource :articles, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
