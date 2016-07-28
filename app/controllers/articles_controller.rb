a169c85af2242ffceeb06d1ab5929aec8bb1dc5c
David Watts <davidjeremywatts@gmail.com>
2016-07-28 15:07:28 -0700

    add index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,13 @@ class ArticlesController < ApplicationController
   end
 
   def index
+    @articles = Article.all
   end
-  def edit
-  end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def good_params
     params.permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
-  root to: 'articles#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  resources :articles, except: [:edit, :update, :destroy]
+
 end
