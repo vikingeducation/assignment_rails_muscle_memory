ea1cc0a10553b10be35356de1b94e920d588a5b7
schoetlr <srlrs20020@gmail.com>
2016-07-28 11:16:33 -0400

    index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
