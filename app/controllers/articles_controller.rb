8cde1562cc3a2a882e29d600870fd7231b6b3c75
David Watts <davidjeremywatts@gmail.com>
2016-07-28 13:23:53 -0700

    add create and show functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,12 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def create
-    article = Article.new(good_params)
-    if article.save
+    @article = Article.new(good_params)
+    if @article.save
       flash[:success] = "Your article has been created!"
-      redirect_to @post
+      redirect_to @article
     else
       flash.now[:error] = "Fix your error!"
       render :new
@@ -19,8 +19,9 @@ class ArticlesController < ApplicationController
   def edit
   end
   def show
+    @article = Article.find(params[:id])
   end
   def good_params
-    params.require(:post).permit(:title, :body
+    params.permit(:title, :body)
   end
 end
