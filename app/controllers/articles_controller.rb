8dfec937a29bba82dea9a40408a9b418874f097a
David Watts <davidjeremywatts@gmail.com>
2016-07-28 12:31:04 -0700

    add create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,25 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def create
+    article = Article.new(good_params)
+    if article.save
+      flash[:success] = "Your article has been created!"
+      redirect_to @post
+    else
+      flash.now[:error] = "Fix your error!"
+      render :new
+    end
+  end
+
   def index
   end
   def edit
   end
   def show
+  end
+  def good_params
+    params.require(:post).permit(:title, :body
   end
 end
