b9e7494f48c13cd91d7d12a80aa9b20fd47dba02
David Watts <davidjeremywatts@gmail.com>
2016-07-28 16:08:53 -0700

    wow scaffolding

---
+++ app/views/schmarticles/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @schmarticles, partial: 'schmarticles/schmarticle', as: :schmarticle
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :schmarticles
   root to: 'articles#index'
   resources :articles
 
