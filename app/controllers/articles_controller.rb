8ff531aa4f41e663909ed41dabd3e879d68ea4f3
BranLiang <lby89757@hotmail.com>
2016-08-15 10:53:25 +0800

    add the show function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,18 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    new_article = Article.new({:title => params[:title], :body => params[:body]})
+    if new_article.save
+      true
+      redirect_to article_path(new_article.id)
+    else
+      false
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
