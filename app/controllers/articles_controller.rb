f7df701972ffcc23d484cb6a69dda21c94694fe6
Tamal Riedman <triedman99@gmail.com>
2016-10-02 17:28:21 -0700

    Update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(whitelisted_post_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
   private
 
   def whitelisted_post_params
