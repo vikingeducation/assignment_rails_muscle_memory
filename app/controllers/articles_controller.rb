670f25d693d5a46b3e2f1857e7985dda80c78c0e
Tamal Riedman <triedman99@gmail.com>
2016-10-02 17:13:11 -0700

    Create and Show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(whitelisted_post_params)
+    @article.save
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def whitelisted_post_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
