8503c1e1dfb21ef18553bf3aaef084245cdde78e
Joe Bernardi <joebernardi@hotmail.com>
2016-10-23 22:48:32 -0500

    create and show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,28 @@
 class ArticlesController < ApplicationController
 
-	def index
-
-	end
+	include ArticlesHelper
 
 	def new
 
+
 	end
+
 
 	def create
 
+		@article = Article.new( article_params )
+
+		@article.save
+
+		redirect_to @article
+
 	end
+
+	def show
+
+		@article = Article.find( params[:id])
+
+	end
+
 
 end
