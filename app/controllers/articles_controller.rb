0dd65aa37f4e1f6eb083bddb873d5efa9a68a51d
Conor Fischer <conor.fischer@gmail.com>
2016-10-27 13:17:50 -0500

    Add Simple navbar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      flash[:success] = "Success Article Created."
       redirect_to article_path(@article)
     else
+      flash[:error] = "Error with article."
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
