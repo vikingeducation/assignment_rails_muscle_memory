f04a647d884e62568dc12502d75974ad85179a71
Conor Fischer <conor.fischer@gmail.com>
2016-10-27 12:41:18 -0500

    Add Article Create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def new
-    
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+    end
   end
 end
