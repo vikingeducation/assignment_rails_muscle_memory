2ab72100cd1c5d8ef9773d7f4bf29e37df88c481
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-22 11:36:08 -0700

    Add create action with strong params and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,21 @@
 class ArticlesController < ApplicationController
 
   def new
-
+    @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      flash.now[:error] = "You must include a title and body."
+      render :new
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
