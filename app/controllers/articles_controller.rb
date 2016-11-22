561600825e7debb0afb77ecfa19e8bc5766f7fcf
Pramod Jacob <psjacob68@gmail.com>
2016-11-22 14:04:05 -0500

    working on delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to 'index'
+  end
+
   private
 
     def strong_params
