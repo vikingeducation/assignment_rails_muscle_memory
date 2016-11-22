b9ef5945401c5ad2505e98dce37cf1d508589c7c
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-22 13:08:00 -0500

    Implement RESTful routes for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,39 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article created!"
+      redirect_to @article
+    else
+      flash[:error] = "Try that one more time"
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-
-
-
   root 'articles#new'
 
   resources :articles
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # get '/articles' => 'articles#index'
-  # get 'articles/new' => 'articles#new'
-
 end
