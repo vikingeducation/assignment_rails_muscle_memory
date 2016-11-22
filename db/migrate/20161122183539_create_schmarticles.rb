a81cfeca79d02baba5db518f766cb482a6552178
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-22 10:41:07 -0800

    finish

---
+++ db/migrate/20161122183539_create_schmarticles.rb
@@ -0,0 +1,10 @@
+class CreateSchmarticles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :schmarticles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
