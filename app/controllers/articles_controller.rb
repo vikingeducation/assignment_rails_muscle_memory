998bcfaa4a866c7879f4d800246259fa783b7fde
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-22 12:10:43 -0700

    restful

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all()
+  end
+
   def new
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(allowed_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'articles#new'
-  resources :articles, only: [:new, :create, :show]
+  root to: 'articles#index'
+  resources :articles, only: [:create, :destroy, :edit, :index, :new, :update, :show]
 end
