4811678df47a6c80a4eb535105879a65d2898d05
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-22 10:37:03 -0800

    navbar and editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,4 +14,22 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.update(title: params[:title], body: params[:body])
+    if @article.save
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
