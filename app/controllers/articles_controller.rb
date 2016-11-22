d5de4033ff0602592441c867d16970e86fd1a8bc
James Harris <computersherpa@gmail.com>
2016-11-22 12:15:56 -0600

    Submit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,15 @@ class ArticlesController < ApplicationController
 
   def new
   end
+
+  def create
+    @article = Article.new(allowed_params)
+    @article.save
+    redirect_to new_article_path
+  end
+
+  private
+    def allowed_params
+      params.require(:article).permit(:title, :body)
+    end
 end
