e99dda6bfb669477024f81168588899a312a0863
John Sawyer <sawyer@sawyermerchant.com>
2016-11-22 13:27:40 -0500

    Form redirects to show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    article = Article.new(title: params[:title], body: params[:body])
-    article.save
+    @article = Article.new(title: params[:title], body: params[:body])
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find( params[:id] )
   end
 
 end
+
+#
+# if @post.save
+#      ... code to set up congratulations message ...
+#      redirect_to post_path(@post.id) # go to show page for @post
+#    else
+#      ... code to set up error message ...
+#      render :new
+#    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root :to => "articles#new"
-  resources :articles, :only => [:new, :create]
+  resources :articles, :only => [:new, :create, :show]
 end
