74a86207cc2a342124de77fb7ab0e4327383ba2a
Pramod Jacob <psjacob68@gmail.com>
2016-11-22 13:53:26 -0500

    added edit feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   private
 
     def strong_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root :to => 'articles#new'
-
   resources :articles
 
 end
