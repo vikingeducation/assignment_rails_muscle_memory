6e6568e88bd12940443dadcba48c74c062585e17
tt <ttwang.303@gmail.com>
2017-01-01 13:50:25 -0500

    add index action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
 
   def new
   end
@@ -15,5 +19,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   root to: "articles#new"
 
-  resources :articles
+  resources :articles, :except => [:update, :destroy, :edit]
 
   #get '/articles/new' => "articles#new"
   #post '/articles/:id' => "articles#create"
-  #get '/articles/:id' => "articles#show" #BUT WHYD OEES THIS NOT FUSFJSLJWER WORK?!@!?@??!
+  #get '/articles/:id' => "articles#show" #BUT WHYD OEES THIS NOT FUSFJSLJWER WORK?!@!?@??! (prefix article_path doesn't show up)
 
 end
