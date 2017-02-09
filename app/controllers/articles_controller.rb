b08b895e47d5582727badd1e60e415d4872a5b0f
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-09 11:46:21 +0800

    Add flash messages and use strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new({title: params[:title], body: params[:body]})
-    redirect_to article_path(@article) if @article.save
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Success! Article created'
+      redirect_to article_path(@article)
+    else
+      flash.now[:error] = 'Article not created: ' +
+        @article.errors.full_messages.join(', ')
+      render :edit
+    end
   end
 
   def show
@@ -22,11 +29,30 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    redirect_to article_path(@article) if @article.update({title: params[:title], body: params[:body]})
+    if @article.update(article_params)
+      flash[:success] = 'Success! Article updated'
+      redirect_to article_path(@article)
+    else
+      flash.now[:error] = 'Article not updated: ' + @article.error.full_messages.join(', ')
+      render :edit
+    end
   end
 
   def destroy
     @article = Article.find(params[:id])
-    redirect_to articles_path if @article.destroy
+    if @article.destroy
+      flash[:success] = 'Success! Article deleted'
+      redirect_to articles_path
+    else
+      flash.now[:error] = 'Article not deleted: ' + @article.error.full_messages.join(', ')
+      render :show
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :body
+    )
   end
 end
