1479107e6ed76e895aad852080ebd3f1030818a8
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-09 10:20:15 +0800

    Add show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,9 @@ class ArticlesController < ApplicationController
     @article = Article.new({title: params[:title], body: params[:body]})
     redirect_to article_path(@article) if @article.save
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 end
