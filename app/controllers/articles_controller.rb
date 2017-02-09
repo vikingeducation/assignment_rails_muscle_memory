0a0c440af87873b1724143887ed135a5ab021dd3
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-09 10:18:35 +0800

    Add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
 
   def new
   end
+
+  def create
+    @article = Article.new({title: params[:title], body: params[:body]})
+    redirect_to article_path(@article) if @article.save
+  end
 end
