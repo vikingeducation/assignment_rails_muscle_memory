209ba0d5b01552f7f5f229615e216113db18d695
Visiona <darbis@gmail.com>
2017-03-09 22:31:30 +0000

    Created create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,24 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(post_params)
+    if @article.save
+      redirect_to @article
+    else
+      false
+    end
+  end
+
+  def show
 
   end
-  
+
+  def post_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
