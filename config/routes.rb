5dd0bac875ec5ed73003229ca0a797c9679e0f9e
David Wiesenberg <david_britta@yahoo.com>
2017-03-14 15:22:14 -0400

    intermediate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  get "/posts" => "posts#index"
+  get "/posts" => "posts#new"
+  get "/posts/new" => "posts#create"
+
 end
