8d9b33d73e69df75ebd5dd98c6e2815c7d37ee15
David Wiesenberg <david_britta@yahoo.com>
2017-03-15 21:50:36 -0400

    final

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,11 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(:title => "title", :body => "body")
-      # Could one have instead: 
-      # @article = Article.new params[:title, :body] OR
-      # @article = Article.new(article_params) with private method   (see below) ?
-
+    @article = Article.new(article_params)
     if @article.save
 #     redirect_to @article  ... this is a short cut
       redirect_to article_path(@article.id)
@@ -24,7 +20,47 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(:id)
+    @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :show
+    end
+
+    # Article.update(article_params)
+    # if @article.save
+    #   redirect_to article_path(@article.id)
+    # else
+    #   render :new
+    # end
+
+  end
+
+# when doing form, remember change the action (update not create)
+
+ def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to articles_path
+  end
+
+
+#
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "articles#new"
-
-  get "/articles" => "articles#index"
-  get "/articles/:id" => "articles#show"
-
-  get "/articles/new" => "articles#new"
-  post "/articles" => "articles#create"
-    # shouldn't it be: 
-    # post "/article/:id" => "articles#create" ?
-
-
-# resources :articles
+  resources :articles
 
 end
-
-# shouldn't it be singular ie "article" ?
