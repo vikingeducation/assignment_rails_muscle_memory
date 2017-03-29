91fe8a94bcac868633c499e66e1f248d103d34e6
efraimmgon <efraimmaugusto@gmail.com>
2017-03-29 09:38:18 -0400

    scaffolgind

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :schmarticles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "articles#index"
   resources :articles, :only => [:new, :create, :show, :index, :edit, :update, :destroy]
