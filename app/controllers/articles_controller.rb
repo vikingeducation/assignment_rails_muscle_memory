6b627f347883d8dedefe4728a9bbc40df620f5d8
efraimmgon <efraimmaugusto@gmail.com>
2017-03-29 08:44:26 -0400

    articles#create: route; controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,16 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    a = Article.new(article_params)
+    if a.save
+      redirect_to :show
+    end
+
+  end
+
+  def article_params
+    params.permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "articles#new"
-  resources :articles, :only => [:new]
+  resources :articles, :only => [:new, :create]
 end
