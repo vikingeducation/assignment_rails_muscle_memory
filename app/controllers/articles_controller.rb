08a56fb8f9e2a0a768e9516cced7edaf453ea87f
Frank Yu <frankyu1@gmail.com>
2017-04-24 10:41:04 -0700

    added create method

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+
+  def new 
+
+  end
+
+  def create
+    @article = Article.new( :title => params[:title], :body => params[:body])
+    if @article.save
+      redirect_to article_path(@article.id)
+    else
+      render :new
+    end
+  end
+
+end
