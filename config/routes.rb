66fd54903ef5e4d33c1137eae900740c6315bb72
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-11 18:23:28 -0400

    Add new article form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'articles#new'
+post '/article' => 'articles#new'
 end
