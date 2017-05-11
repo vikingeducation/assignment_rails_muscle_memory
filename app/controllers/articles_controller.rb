2ea70ac9737f99e7b780ba83b6db8cc1a9312d5b
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-11 19:22:47 -0400

    Add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:title, :body])
+    @article.save
+    if @article.save
+      redirect_to @article.id
+    else
+      render :new
+    end
   end
 end
