f1bd4b619b9dcb99cee01a611c00348dee9a2789
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-15 09:59:45 -0400

    Add nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'articles#new'
-resources :articles, :only => [:new, :show, :create]
+resources :articles, :only => [:new, :show, :create, :index]
 # get '/articles/:id' => 'articles#show'
 # post '/article/new' => 'articles#new'
 # post '/article' => 'articles#create'
