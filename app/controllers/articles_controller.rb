82f0a7812fe22f9d5502ce68cd4b0760a1812ed9
Roy Chen <roychenlei@gmail.com>
2017-06-11 16:05:29 +0800

    implemented articles#create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
 
-  def new; end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(whitelisted_article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+
+    end
+  end
+
+  private
+
+  def whitelisted_article_params
+    params.permit(:title, :body)
+  end
 end
