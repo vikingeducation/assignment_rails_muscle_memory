88dc81a26da60d3952222720d9ad5e7057a51665
andie5 <andieasa@googlemail.com>
2017-07-09 00:41:37 +0100

    Edit article built

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(whitelisted_post_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
 
 end
