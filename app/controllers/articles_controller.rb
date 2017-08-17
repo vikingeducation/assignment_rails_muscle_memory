5ff0105a6ea50296bc31d759115322575e7f800a
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-17 13:32:09 -0600

    added show route and view, can now submit article, displays after submit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,21 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def new
   end
-  
+
+  def create
+    @article = Article.new(:title => params[:title], :body => params[:body])
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'articles#new'
-  get '/articles/new' => "articles#new"
-  
-
+  resources :articles, :only => [:new, :create, :show]
 end
