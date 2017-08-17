6710faa2624d41a748aca2fe857d6c59fb010419
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-17 13:49:38 -0600

    added index and nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'articles#new'
-  resources :articles, :only => [:new, :create, :show]
+  resources :articles, :only => [:new, :create, :show, :index]
 end
