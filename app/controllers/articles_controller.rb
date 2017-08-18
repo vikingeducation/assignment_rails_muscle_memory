76ef99fb5ed61dc516b644f4b83e656e7d3a1c94
Sean Kelly <shonkelli@gmail.com>
2017-08-17 23:04:35 -0400

    added edit and update routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,15 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_parameters)
+    redirect_to article_path(@article)
   end
 end
