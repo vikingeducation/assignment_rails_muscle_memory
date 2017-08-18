06d7f36c55241ce6b81b972f94b45197b6c3317c
Sean Kelly <shonkelli@gmail.com>
2017-08-18 10:24:51 -0400

    added and tested destroy route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,4 +28,10 @@ class ArticlesController < ApplicationController
     @article.update(article_parameters)
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
