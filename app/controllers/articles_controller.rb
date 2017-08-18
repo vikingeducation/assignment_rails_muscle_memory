c8796b4c7e12521e7aeaa830b722a4c1cffeb7a3
Sean Kelly <shonkelli@gmail.com>
2017-08-17 22:47:47 -0400

    added navbar and index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
 
-  # def index
-  #   @articles = Article.all
-  # end
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -17,6 +17,6 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    
+
   end
 end
