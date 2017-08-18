5791e5686e145b47795ff7b06da439f9bb7e5ffa
Sean Kelly <shonkelli@gmail.com>
2017-08-17 22:29:38 -0400

    added create controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   # def index
   #   @articles = Article.all
   # end
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_parameters)
+    @article.save
+    redirect_to article_path(@article)
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,3 +1,5 @@
 module ArticlesHelper
-  
+  def article_parameters
+    params.require(:article).permit(:title, :body)
+  end
 end
