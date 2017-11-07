3c767cae1d34729455d84a03420ff89fcc23dd83
anne <richardson.ae@gmail.com>
2017-11-07 07:36:58 -0600

    Reorders action methods to match scaffold

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,18 @@ class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
   end
 
   def create
@@ -15,17 +25,6 @@ class ArticlesController < ApplicationController
       flash.now[:alert] = "Whoopsie!"
       render :new
     end
-
-  end
-
-  def index
-    @articles = Article.all
-  end
-
-  def show
-  end
-
-  def edit
   end
 
   def update
